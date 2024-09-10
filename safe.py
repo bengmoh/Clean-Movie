c14ed4d8cb1f7b2d834983751b6ad114f0295d7e
bengmoh <bengueddache.mohamedelamine@gmail.com>
2024-09-09 20:32:04 -0400

    moved encoding script outside of working directory

--- safe.py
+++
@@ -1,9 +0,0 @@
-import base64
-
-def encode(interval_string):
-    encoded_string = base64.b85encode(interval_string.encode()).decode()
-    return encoded_string
-
-def decode(code):
-    decoded_string = base64.b85decode(code).decode()
-    return decoded_string
